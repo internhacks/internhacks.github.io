d9cba04dd991bd3427166adf6f030700ea2f9273
Kelsey Hrubes <kelseyhrubes@gmail.com>
2015-07-07 15:50:20 -0700

    remove formcontrol.js

--- assets/js/formcontrol.js
+++
@@ -1,7 +0,0 @@
-
-$('#contact-us-submit-button').click(function(event){
-	console.log(event);
-	console.log($('#contact-us-form').serializeArray());
-	//$("#contact-us-modal").closeModal();
-	event.preventDefault();
-});
