8ba82893d3ce9a45b51331d474f2b14743abc683
Kelsey Hrubes <kelseyhrubes@gmail.com>
2015-07-08 23:42:02 -0700

    update schedule widget

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -17,23 +17,20 @@ function isMobile() {
 
 //implicit iteration is cool huh? <3 jQuery
 $(".card").addClass('hoverable');
-$(".card").addClass('blue-grey lighten-5');
-
-
 
 var schedule;
 
 function initializeSchedule(){
 	schedule = [
-		{time:"9-10", id:"w1", title:"Start Hacking!", companyName:"", instructor:"", text:""},
-		{time:"10-11", id:"w2", title:"Intro to HTML/CSS", companyName:"Google", instructor:"Manu Cornett", text:"This workshop will get you up to speed with the basics of HTML/CSS, to get you ready for a successful day of hacking!"},
-		{time:"11-12", id:"w3", title:"How Host a Site with Namecheap", companyName:"Namecheap", instructor:"", text:"We will be bringing you an expert from Namecheap to teach you how to get your beautiful portfolio site hosted!"},
+		{time:"9-10", id:"w1", title:"Start Hacking!", companyName:"", img:"<img src='assets/img/internhacks-blue.png'>", instructor:"", text:""},
+		{time:"10-11", id:"w2", title:"Intro to HTML/CSS", companyName:"Google", img:"<img src='assets/img/htmlcss.png'>", instructor:"Manu Cornett", text:"This workshop will get you up to speed with the basics of HTML/CSS, to get you ready for a successful day of hacking!"},
+		{time:"11-12", id:"w3", title:"How Host a Site with Namecheap", companyName:"Namecheap", img:"<img src='assets/img/namecheap-logo.png'>", instructor:"", text:"We will be bringing you an expert from Namecheap to teach you how to get your beautiful portfolio site hosted!"},
 		{time:"12-1", id:"w4", title:"LUNCH", companyName:"", instructor:"", text:""},
-		{time:"1-2", id:"w5", title:"Weebly Workshop", companyName:"Weebly", instructor:"", text:"Join us for this workshop presented by an engineer from <a href='http://www.weebly.com/'>Weebly</a>! <br>More info coming soon!"},
-		{time:"2-3", id:"w6", title:"Kik Workshop", companyName:"Kik", instructor:"instructor", text:"Join us for this workshop presented by an engineer from <a href='http://www.kik.com/'>Kik</a>! <br>More info coming soon!"},
-		{time:"3-4", id:"w7", title:"AngularJS Workshop", companyName:"<a href='http://warehousing.theshotput.com/'>The Shotput</a>", instructor:"James Steinberg", text:"Ever wanted to learn AngularJS? This framework will give your portfolio a beautiful look and feel."},
-		{time:"4-5", id:"w8", title:"Upcoming Dev tools", companyName:"<a href='http://joelcox.io/'>joelcox.io</a>", instructor:"Joel Cox", text:"More info coming soon!"},
-		{time:"5pm", id:"w9", title:"Submit your hack!", companyName:"", instructor:"", text:"That's right! While InternHacks isn't a traditional hackathon, we decided to reward those who put in a lot of time and effort with <br><b>BEST PORTFOLIO WEBSITE</b></br> determined by our expert judging panel."},
+		{time:"1-2", id:"w5", title:"Weebly Workshop", companyName:"Weebly", instructor:"", img:"<img src='assets/img/weebly-logo.gif'>", text:"Join us for this workshop presented by an engineer from <a href='http://www.weebly.com/'>Weebly</a>! <br>More info coming soon!"},
+		{time:"2-3", id:"w6", title:"Kik Workshop", companyName:"Kik", instructor:"instructor", img:"<img src='assets/img/kik-logo.png'>", text:"Join us for this workshop presented by an engineer from <a href='http://www.kik.com/'>Kik</a>! <br>More info coming soon!"},
+		{time:"3-4", id:"w7", title:"AngularJS Workshop", companyName:"<a href='http://warehousing.theshotput.com/'>The Shotput</a>", instructor:"James Steinberg", img:"<img src='assets/img/AngularJS.png'>", text:"Ever wanted to learn AngularJS? This framework will give your portfolio a beautiful look and feel."},
+		{time:"4-5", id:"w8", title:"Upcoming Dev tools", companyName:"<a href='http://joelcox.io/'>joelcox.io</a>", instructor:"Joel Cox", img:"<img src='assets/img/favicon-@2x.png'>", text:"More info coming soon!"},
+		{time:"5pm", id:"w9", title:"Submit your hack!", companyName:"", instructor:"", img:"<img src='assets/img/internhacks-blue.png'>", text:"That's right! While InternHacks isn't a traditional hackathon, we decided to reward those who put in a lot of time and effort with <br><b>BEST PORTFOLIO WEBSITE</b></br> determined by our expert judging panel."},
 	];
 }
 
@@ -68,11 +65,11 @@ function addCollapseSchedule(){
 }
 
 function addTabSchedule(){
-	$(".schedule").append('<div class="card z-depth-4 col s12 schedule-tabs"><h2>{ Schedule }</h2><ul class="tabs"></ul></div>');
+	$(".schedule").append('<div class="card z-depth-4 col s12 schedule-tabs hide-on-med-and-down"><h2>{ Schedule }</h2><ul class="tabs"></ul></div>');
  
 	for(var i=0; i<getSchedule().length; i++){
 		var tabElem = "<li class="+"tab col s1"+"><a href="+"#"+getSchedule()[i].id+">"+getSchedule()[i].time+"</a></li>";
-		var dataElem = "<div class="+"'schedule-item col s12'"+" id="+"'"+getSchedule()[i].id+"'"+"><h4>"+getSchedule()[i].title+"</h4>"+getInstructorElement(getSchedule()[i])+"<p>"+ getSchedule()[i].text +"</p></div>";
+		var dataElem = "<div class="+"'schedule-item col s12'"+" id="+"'"+getSchedule()[i].id+"'"+"><h4>"+getSchedule()[i].title+"</h4>"+getInstructorElement(getSchedule()[i])+"<p>"+ getSchedule()[i].text +"</p>"+ getImg(getSchedule()[i]) +"</div>";
 
 		$(".tabs").append(tabElem);
 		$(".schedule-tabs").append(dataElem);
@@ -89,9 +86,17 @@ function getInstructorElement(scheduleObj){
 	return elem;
 }
 
+function getImg(scheduleObj){
+	var img=""
+	if(scheduleObj.img){
+		img += scheduleObj.img;
+	}
+	console.log(img);
+	return img;
+}
+
 
 $( document ).ready(function(){
 	//$('.card').addClass('hoverable');
 	addScheduleToPage();
-	console.log(getSchedule());
 });
